c1dcfcf12b15f915f7c333af193d56c5d090c205
Abhishek Madan <abhishekmadan60@gmail.com>
2018-02-19 18:14:35 -0800

    Designing dashboard

---
+++ src/CMPE295-enterprise-grade-botman/public/scripts/landingpage.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $("#add-bot").click(function(){
+        $("#bot-create-model-container").show();
+    });
+
+    $("#close-bot-model").click(function(){
+        $("#bot-create-model-container").hide();
+    });
+});
