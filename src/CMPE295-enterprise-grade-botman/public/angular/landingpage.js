95a21a8e3ed2ff0d4542533426039cb5ef673858
Abhishek Madan <abhishekmadan60@gmail.com>
2018-04-07 21:47:47 -0700

    Adding create bot dialogue.

--- src/CMPE295-enterprise-grade-botman/public/angular/landingpage.js
+++ src/CMPE295-enterprise-grade-botman/public/angular/landingpage.js
@@ -1,7 +1,11 @@
 var app = angular.module('LandingPage',[]);
 app.controller('BotInfo',function($scope,$http){
+
     $scope.showDialogueDeleteBot = false;
+    $scope.deleteBotWithId =-1;
+
     //$http.get("")
+
     $scope.botList =[
         {
             botId:1,
@@ -29,17 +33,29 @@ app.controller('BotInfo',function($scope,$http){
         }
     ];
 
-    $scope.editBot = function(bId) {
-        console.log(bId);
+    $scope.cancelRequestDeleteBot = function () {
+        $scope.showDialogueDeleteBot = false;
+        $scope.deleteBotWithId = -1;
     }
 
     $scope.deleteBot = function(bId) {
-        console.log(bId);
-        for (i = 0; i<botList.length(); i++) {
-            if (botList[i].botId===bId) {
-
+        var li = $scope.botList;
+        for (i = 0; i<li.length; i++) {
+            if (li[i].botId===bId) {
+                $scope.deleteBotWithName = li[i].name;
+                $scope.deleteBotWithId = li[i].botId;
+                $scope.showDialogueDeleteBot = true;
             }
         }
-        console.log($scope.botList[bId-1]);
+    }
+
+    $scope.sendRequestDeleteBot = function() {
+        if ($scope.deleteBotWithId>0) {
+            //send request
+        }
+    }
+
+    $scope.sendRequestEditBot = function(bId) {
+        console.log(bId);
     }
 });
