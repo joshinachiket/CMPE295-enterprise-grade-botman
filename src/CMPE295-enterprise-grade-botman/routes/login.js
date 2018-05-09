8fd5fdead095849609fccff2bcb2aab4c39d9ddc
shettyaditi <aditi.shetty@sjsu.edu>
2018-05-08 18:38:38 -0700

    Implemented some styling changes

--- src/CMPE295-enterprise-grade-botman/routes/login.js
+++ src/CMPE295-enterprise-grade-botman/routes/login.js
@@ -103,6 +103,7 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     console.log(req.user.username);
     req.session.username = req.user.username;
+
     return next();
   } else {
     console.log("not authenticated");
